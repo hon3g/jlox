b8d0607f5e98b70a5845c2aacee68f3fc84854de
hon3g <veston.zhao@gmail.com>
2022-03-31 15:58:36 -0400

    Generate Expression class

--- src/com/craftinginterpreters/lox/Scanner.java
+++ src/com/craftinginterpreters/lox/Scanner.java
@@ -1,6 +1,5 @@
 package com.craftinginterpreters.lox;
 
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -214,12 +213,3 @@ public class Scanner {
         tokens.add(new Token(type, text, literal, line));
     }
 }
-
-
-
-
-
-
-
-
-
